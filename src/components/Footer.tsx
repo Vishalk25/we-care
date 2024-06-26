d0f36dd532dd798460a1c1c11b766c54c3a8e928
mastery-vishal-kori <vishalkori1995@gmail.com>
2024-06-26 18:37:07 +0530

    initial commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+import "./Footer.css";
+
+export const Footer = () => {
+  return (
+    <>
+      <div id="footer">
+        <div className="footer-text">About Us</div>
+        <div className="footer-text">FAQ's</div>
+        <div className="footer-text">Career</div>
+        <div className="footer-text">Fundraise Support</div>
+      </div>
+      <div id="copyright">
+        Copyright ©2024 All rights reserved | This template is made by Vishal
+      </div>
+    </>
+  );
+};
