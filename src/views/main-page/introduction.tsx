099b44b85e2733fc724bc04bc2049a3bda194b22
mastery-vishal-kori <vishalkori1995@gmail.com>
2024-06-27 19:25:18 +0530

    added introduction section

---
+++ src/views/main-page/introduction.tsx
@@ -0,0 +1,31 @@
+import './introduction.css';
+
+
+export const Introduction = () => {
+  return (
+    <>
+    <div id="intro-main">
+    <div id='intro-desc'>
+      <h3>What we do</h3>
+      <div>
+        We-Care India, is an award winning NGO that stands for the economic and
+        social empowerment of women and children. We-Care started in 2020 with the intent of
+        liberating young women and children, through the pursuit of STEM education. The
+        initiative prepares young women for leadership roles, thereby creating a
+        wider talent pool and helping bridge the gender divide. We-Care
+        achieves this objective through a blend of unique development
+        interventions, including our 600 hour proprietary and scientifically
+        researched curriculum. Over their four years in the program, we also
+        provide one-to-one mentorship, access to best in class technology,
+        assistance with internships, and world class industry exposure and
+        corporate interactions. We-Care’s vision is to empower women and children for
+        equality in opportunity and leadership roles.
+      </div>
+    </div>
+    <div id='intro-img'>
+        <img src="../../../src\assets\main-page4.png" alt='homeless-people' height={'350px'} width={'450px'} />
+    </div>
+    </div>
+    </>
+  );
+};
